eed2d586e70e1f6c5c4631f5bace8d5268326aeb
pradeep-verma <pradeep.verma.5dec@gmail.com>
2022-02-15 20:57:30 +0000

    first commit

---
+++ src/components/PlanetItem/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const PlanetItem = props => {
+  const {planetDetails} = props
+  const {name, imageUrl, description} = planetDetails
+
+  return (
+    <div className="planet-item-container">
+      <img src={imageUrl} alt={`planet ${name}`} className="planet-image" />
+      <h1 className="planet-name">{name}</h1>
+      <p className="planet-description">{description}</p>
+    </div>
+  )
+}
+export default PlanetItem
---
+++ src/components/PlanetsSlider/index.js
@@ -0,0 +1,30 @@
+import Slider from 'react-slick'
+
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+import PlanetItem from '../PlanetItem'
+
+import './index.css'
+
+const PlanetsSlider = props => {
+  const {planetsList} = props
+  const settings = {
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
+
+  return (
+    <div className="slider-container" testid="planets">
+      <h1 className="planets-title">PLANETS</h1>
+      <Slider {...settings}>
+        {planetsList.map(eachPlanet => (
+          <PlanetItem key={eachPlanet.id} planetDetails={eachPlanet} />
+        ))}
+      </Slider>
+    </div>
+  )
+}
+
+export default PlanetsSlider
